f714e78ec3d096aeac9f33f64375d9373edc8b63
melike çelik <mlkclke1@gmail.com>
2023-08-01 00:41:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+
+    public static boolean asalMi(int sayi, int bolen){
+        if (sayi < 2) {
+            return false;
+        }
+        if (sayi == 2){
+            return true;
+        }
+        if (sayi % bolen == 0) {
+            return false;
+        }
+        if (bolen * bolen > sayi){
+            return true;
+        }
+        return asalMi(sayi, bolen +1);
+    }
+
+    public static void main(String[] args){
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir sayi girinizi :");
+        int sayi = inp.nextInt();
+
+        if (asalMi(sayi,2)){
+            System.out.println(sayi + " asal bir sayıdır.");
+        } else {
+            System.out.println(sayi + " asal bir sayı değildir.");
+        }
+
+
+    }
+}
